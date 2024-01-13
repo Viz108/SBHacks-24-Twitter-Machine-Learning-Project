b1c764eae8a3f867f26e4c8382dddbbcd7403315
Viz108 <67886538+Viz108@users.noreply.github.com>
2024-01-12 22:13:14 -0800

    Groundwork for machine learning model

--- model.py
+++ model.py
@@ -2,4 +2,10 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-data = pd.read_csv("Twitterdatainsheets.csv")
+#data = pd.read_csv("Twitterdatainsheets.csv", dtype={'index': int, 'TweetID': 'string', 'Weekday': 'string', 'Hour': int, 'Day': int, 'lang': 'string', 'IsReshare': bool, 'Reach': float, 'RetweetCount': float, 'Likes': float, 'Klout': float, 'Sentiment': float, 'text': 'string', 'LocationID': float, 'UserID': bool})
+data = pd.read_csv("Twitterdatainsheets.csv", low_memory=False)
+
+print(data)
+print(data.columns.tolist())
+print(data[' Day'])
+
